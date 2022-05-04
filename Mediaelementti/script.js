3240fc80152f7263564b49f0b740752665832115
Miro Miettinen <miro.miettinen@student.lab.fi>
2022-05-04 19:30:58 +0300

    3.5.2022

---
+++ Mediaelementti/script.js
@@ -0,0 +1,7 @@
+var previous = document.getElementById("bi bi-arrow-left-square-fill");
+var next = document.getElementById("bi bi-arrow-right-square-fill");
+var ppbutton = document.getElementById("bi bi-play-circle-fill");
+
+function prevNext (){
+    
+}
---
+++ tentti/script.js
@@ -0,0 +1,6 @@
+function hide(id) {
+	document.getElementById(id).style.display = "none";
+}
+function show(id) {
+	document.getElementById(id).style.display = "";
+}
